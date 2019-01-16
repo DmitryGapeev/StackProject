f60cd0e9545ad8a3ab3b594d4251850a817742da
Гапеев Дмитрий Игоревич <Dmitriy.Gapeev@galaktika.by>
2019-01-16 10:57:36 +0300

    Add project files.

---
+++ StackProject/Program.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsDataStructures
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			TestStack();
+
+			string string1 = "(()((())()))";
+			string string2 = "(()()(())";
+			string string3 = "())(";
+			string string4 = "))((";
+
+			bool isBalance = IsBalanceBrackets(string1);
+			Console.WriteLine(string1 + " is balance = " + isBalance);
+
+			isBalance = IsBalanceBrackets(string2);
+			Console.WriteLine(string2 + " is balance = " + isBalance);
+
+			isBalance = IsBalanceBrackets(string3);
+			Console.WriteLine(string3 + " is balance = " + isBalance);
+
+			isBalance = IsBalanceBrackets(string4);
+			Console.WriteLine(string4 + " is balance = " + isBalance);
+
+			string expression1 = "1 2 + 3 * =";
+			string expression2 = "8 2 + 5 * 9 + =";
+
+			Console.WriteLine("compute expression");
+			int result1 = ComputePostfixExpression(expression1);
+			Console.WriteLine(expression1 + " " + result1);
+			int result2 = ComputePostfixExpression(expression2);
+			Console.WriteLine(expression2 + " " + result2);
+
+			Console.ReadKey();
+		}
+
+		static void TestStack()
+		{
+			Console.Write("test push");
+			Console.WriteLine();
+
+			Stack<int> stack = new Stack<int>();
+
+			Console.Write("push into stack ");
+			for (int i = 1; i <= 10; i++)
+			{
+				Console.Write(i + " ");
+				stack.Push(i);
+			}
+
+			Console.WriteLine();
+			Console.WriteLine("size after push = " + stack.Size());
+
+			Console.WriteLine();
+			Console.WriteLine("test peek");
+			int peekResult = stack.Peek();
+			Console.WriteLine("peek result = " + peekResult);
+			Console.WriteLine("size after peek = " + stack.Size());
+
+			Console.WriteLine();
+			Console.WriteLine("test pop");
+
+
+			Console.Write("pop from stack ");
+			for (int i = 1; i <= 10; i++)
+			{
+				int item = stack.Pop();
+				Console.Write(item + " ");
+			}
+			Console.WriteLine();
+			Console.WriteLine("size after pop = " + stack.Size());
+
+			Console.WriteLine();
+			Console.WriteLine("try to pop, size = 0");
+			int result = stack.Pop();
+			Console.WriteLine("pop result = " + result);
+		}
+
+		static bool IsBalanceBrackets(string row)
+		{
+			Stack<char> stack = new Stack<char>();
+			bool result = true;
+			foreach (char bracket in row)
+			{
+				if(bracket == '(')
+					stack.Push(bracket);
+				else if (bracket == ')' && stack.Size() != 0)
+					stack.Pop();
+				else
+				{
+					result = false;
+					break;
+				}
+			}
+
+
+			return result;
+		}
+
+		static int ComputePostfixExpression(string expression)
+		{
+			Stack<string> stack1 = new Stack<string>();
+			Stack<int> stack2 = new Stack<int>();
+
+			string[] expressionItems = expression.Split(' ');
+
+			foreach (string expItem in expressionItems.Reverse())
+				stack1.Push(expItem);
+
+			while (stack1.Size() != 0)
+			{
+				string expressionItem = stack1.Pop();
+
+				if (expressionItem == "*")
+				{
+					if(stack2.Size()<2)
+						throw new InvalidOperationException("недостаточно значений для умножения");
+
+					int val1 = stack2.Pop();
+					int val2 = stack2.Pop();
+					stack2.Push(val1 * val2);
+				}
+				else if (expressionItem == "+")
+				{
+					if (stack2.Size() < 2)
+						throw new InvalidOperationException("недостаточно значений для сложения");
+
+					int val1 = stack2.Pop();
+					int val2 = stack2.Pop();
+					stack2.Push(val1 + val2);
+				}
+				else if(expressionItem == "=")
+					break;
+				else
+				{
+					stack2.Push(int.Parse(expressionItem));
+				}
+			}
+
+			if(stack2.Size() > 1)
+				throw new InvalidOperationException("не удалось вычислить выражение");
+
+			return stack2.Pop();
+		}
+	}
+}
---
+++ StackProject/Stack.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace AlgorithmsDataStructures
+{
+
+	public class Stack<T>
+	{
+		private readonly List<T> _list;
+		public Stack()
+		{
+			_list = new List<T>();
+		}
+
+		public int Size()
+		{
+			return _list.Count;
+		}
+
+		public T Pop()
+		{
+			if (_list.Count == 0)
+				return default(T);
+
+			T result = _list.Last();
+			_list.RemoveAt(_list.Count-1);
+			return result;
+		}
+
+		public void Push(T val)
+		{
+			_list.Add(val);
+		}
+
+		public T Peek()
+		{
+			if (_list.Count == 0)
+				return default(T);
+
+			return _list.Last();
+		}
+	}
+
+}
